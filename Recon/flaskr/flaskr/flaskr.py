e56858441d74260279d94fb754972ba95edb11f6
Fatme Fawaz <fatme@ssoc18.teamshadi.net>
2018-08-01 10:55:45 +0000

    modififed app execution

--- Recon/flaskr/flaskr/flaskr.py
+++ Recon/flaskr/flaskr/flaskr.py
@@ -25,6 +25,7 @@ from sqlalchemy import create_engine, MetaData, Table
 from sqlalchemy.orm import mapper, sessionmaker
 from sqlalchemy import Column, Integer
 from sqlalchemy.dialects.postgresql import insert
+
 import logging
 logging.getLogger('sqlalchemy.dialects.postgresql').setLevel(logging.INFO)
 
@@ -33,31 +34,15 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_FOLDER = os.path.join(BASE_DIR, 'Upload Folder')
 ALLOWED_EXTENSIONS = (['csv'])
 
-
 app = Flask(__name__)
 app.secret_key = "super secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config.from_object('flaskr.settings') # This will refer to settings.py
 
 
 class securities(object):
     pass
  
-
-# def loadSession():
-#     """"""    
-    
-#     engine = create_engine('postgresql:/fatme:upBH2UtS@localhost/mf_dummy', echo=True)
- 
-#     metadata = MetaData(engine)
-#     sec = Table('securities', metadata, 
-#                       Column("symbol", Integer, primary_key=True), Column("position", Integer, primary_key=True))
-#     mapper(Securities, sec)
- 
-#     Session = sessionmaker(bind=engine)
-#     session = Session()
-#     return session
- 
-
 
 def timestamp():
     ts = time.time()
@@ -136,7 +121,7 @@ def Recon():
             return redirect(request.url)
 
     fn_sel = Time
-    return redirect("http://ssoc18.teamshadi.net:5000/ReconView?fn_sel=" + fn_sel)
+    return redirect("/ReconView?fn_sel=" + fn_sel)
   
 
 @app.route('/ReconView', methods=['GET', 'POST'])
@@ -148,17 +133,25 @@ def Redux():
             f1=file
             f11 = os.path.join(app.config['UPLOAD_FOLDER'], f1)
             rc1 = pd.read_csv(f11)
+            rc1['position'] = rc1['position'].astype('int')
+            rc1['symbol'] = rc1['symbol'].astype('str')
 
         elif file.endswith(num + "-f2.csv"):
             f2=file
             f22 = os.path.join(app.config['UPLOAD_FOLDER'], f2)
             rc2 = pd.read_csv(f22)
+            rc2['position'] = rc2['position'].astype('int')
+            rc2['symbol'] = rc2['symbol'].astype('str')
+            rc2 = rc2[['symbol','position']]
+            
+    print('rc1',rc1)
+    print('rc2',rc2)
     DF=rc1.merge(rc2, left_on='symbol', right_on='symbol', how='outer')
     DF['diff']=DF['position_x']-DF['position_y']
     x = pd.DataFrame(DF)
     mypath = UPLOAD_FOLDER
     
-    return render_template("ReconView.html", Title="Current Reconciliation", data=x.to_html(), Uploadnew="Upload new reconciliation", H2="View Previous Reconciliations")
+    return render_template("ReconView.html", Title="Current Reconciliation", data=x.to_html(), Uploadnew="Upload new reconciliation", H2="View Previous Reconciliations", vsdatabase="Upload with database")
 
 @app.route('/listprev', methods=['GET', 'POST'])
 def listfiles():
@@ -167,7 +160,7 @@ def listfiles():
     fn_all = [f.replace('.csv','').replace('-f1','').replace('-f2','') for f in fn_all]
     fn_all = list(set(fn_all))
     fn_all.sort()
-    return render_template("listprev.html", fn_all=fn_all, fn_sel=request.args.get('fn_sel'))
+    return render_template("listprev.html", fn_all=fn_all, fn_sel=request.args.get('fn_sel'), H3="Upload from Database" )
 
 @app.route ('/uploadfromdb', methods=['GET','POST'])
 def dbupload():
@@ -197,36 +190,18 @@ def dbupload():
     filename1b = os.path.join(app.config['UPLOAD_FOLDER'], FT1)
     file1.save(filename1b)
     dfrc1 = pd.read_csv(filename1b)
-
     
-#     if 'Table' not in request.headers:
-#         flash('No Selected Table')
-#         return redirect(request.url)
-    Tablename = request.form['Table']
-
-    # if user does not select file, browser also
-    # submit a empty part without filename
-
-    
-    
-#     if 'Table' not in database
-#         flash("Table not in database")
-#         return redirect(request.url)
-
-    tablename=Tablename
-#     engine = create_engine('postgresql:/fatme:upBH2UtS@localhost/mf_dummy', echo=True)
-    engine = create_engine("postgres:http://ssoc18.teamshadi.net:8000/mf_dummy", echo=True)
+    engine = create_engine(app.config['SQLALCHEMY_URI'], echo=True)
     metadata = MetaData(engine)
-    sec = Table('securities', metadata, 
-                      Column("symbol", Integer, primary_key=True), Column("position", Integer, primary_key=True))
-    mapper(Securities, sec)
-    connection = engine.connect()
-    table=pd.read_sql_table(tablename, connection, schema=None, index_col=None, coerce_float=True, parse_dates=None, columns=['symbol','position'], chunksize=None)
+    
+    table=pd.read_sql_table("securities", engine)
     fileT2= Time + '-f2.csv'
-    newfile=table.to_csv('newfile.csv')
+    #newfile=table.to_csv('newfile.csv')
     filename2b = os.path.join(app.config['UPLOAD_FOLDER'], fileT2)
-    newfile.save(filename2b)
-    dfrc2 = pd.read_csv(filename2b)
+    #newfile.save(filename2b)
+    table.to_csv(filename2b)
+    #dfrc2 = pd.read_csv(filename2b)
+    dfrc2=table
 
     #This section checks if any or both files have the appropriate columns and flashes a messages accordingly
     if not (({'symbol', 'position'}.issubset(dfrc1.columns))or({'symbol', 'position'}.issubset(dfrc2.columns))):
@@ -241,7 +216,7 @@ def dbupload():
             return redirect(request.url)
 
     fn_sel = Time
-    return redirect("http://ssoc18.teamshadi.net:5000/ReconView?fn_sel=" + fn_sel)
+    return redirect("/ReconView?fn_sel=" + fn_sel)
     
     
                            
